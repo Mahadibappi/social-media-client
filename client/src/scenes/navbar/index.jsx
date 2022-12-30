842b0fd0815302ede54be5229fdc48eddeb32dd6
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2022-12-30 21:15:44 +0600

    local change

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          SafeMedia
+          Sociopedia
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
