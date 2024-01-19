4097af425e2746c6ef3cac1b7c8b654826217ed5
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2024-01-19 23:55:39 +0600

    heading change

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          Mymedia
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
